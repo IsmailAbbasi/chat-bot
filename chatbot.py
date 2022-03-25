7170d8c843fa94cf95e282a1f6784e3cb5903f42
Ismail Abbasi <ismailabbasi118@gmail.com>
2022-03-25 19:57:39 +0530

    Minor Upgrade added mem bot

--- chatbot.py
+++ chatbot.py
@@ -5,6 +5,7 @@ import time
 import random
 import wikipedia
 from deep_translator import GoogleTranslator
+import pyjokes
 
 app = Flask(__name__)
 
@@ -27,14 +28,14 @@ def send_message():
             'Hello','Hola','Wassup Human'
         ])
     elif message in ['help']:
-        bot_response = "Hey i can do multiple tasks : <br> I can play a song for you <br> I can make you laugh <br> I can Talk to you in any language you like <br> I can tell about you anything you ask for . <br> I can give you movie suggestions."
+        bot_response = "Hey i can do multiple tasks : <br> I can play a song for you <br> I can make you laugh by telling you jokes/memes   <br> I can Talk to you in any language you like <br> I can tell about you anything you ask for . <br> I can give you movie suggestions."
     elif message in ['how are you', 'wassup']:
-        bot_response = "I am fine."
+        bot_response = "I am fineeeee."
     elif all(x in message for x in 'who made you'.split(" ")) or all(x in message for x in 'how you born'.split(" ")):
-        bot_response = "I was sent to earth by gods of the middle earth <br>.<br>.<br>Abey saale! Kisi Developer ne hi banaya hoga na 😠 <br> :P <br>"
-    elif all(x in message for x in ['how','are','you']):
-        bot_response = "I am better then before!"
-    elif all(x in message for x in ['what','you','doing']):
+        bot_response = "I was sent to earth by gods of the middle earth <br>.<br>.<br>Abey saale! Kisi Developer ne hi banaya hoga na 😠 <br> maaf krna gusse me thoda idhar udhar nikal jaata hoon "
+    # elif all(x in message for x in ['how','are','you']):
+    #     bot_response = "I am better then before!"
+    elif all(x in message for x in ['what','you','doing']) or all(x in message for x in ['how','you','doing']):
         bot_response = "Trying to do better ;)<br>(Spiderman reference)"
     elif all(x in message for x in ['play','song']):
         bot_response = 'Playing a random song on youtube from my favourite playlist'
@@ -49,6 +50,7 @@ def send_message():
             bot_response = wikipedia.summary(topic)
         except:
             bot_response = "Sorry, I don't know anything about " + topic + "!"
+
     elif message.startswith("talk to me in"):
         language = message.replace("talk to me in ", "")
         response_type = 'change_langauge'
@@ -56,12 +58,20 @@ def send_message():
         current_language = language
         bot_response = "Ok! From now on I will talk to you in " + language.title() + "."
 
+    elif all(x in message for x in ['tell','me','a','joke',]) or all(x in message for x in ['tell','me','a','meme']) or all(x in message for x in ['make','me','laugh']): 
+    # message.startswith("meme") or message.startswith("tell me a joke") or message.startswith("make me laugh") or message.startswith("joke"):
+        # topic = message.replace("meme", "").replace("tell me a joke", "").replace("make me laugh","").replace("can you tell me a", "")
+        bot_response = pyjokes.get_joke()  
+        # bot_response = print(pyjokes.get_joke())
+
     if current_language != 'english':
         translator = GoogleTranslator(source='auto', target=current_language)
         bot_response = translator.translate(bot_response)
+        
 
     response = {"response":bot_response, "response_type":response_type, "action":action}
     return jsonify(response)
+
 
     
 if __name__  == '__main__':
